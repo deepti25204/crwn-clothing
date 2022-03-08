42d81d81b519b32f77e355a59d52e11ba697f78f
Deepti Aggarwal <deeaggar3@publicisgroupe.net>
2022-03-08 21:44:26 +0530

    Google Sign in - Firebase

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/compat/app';
+
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+const config = {
+  apiKey: "AIzaSyB4dv6XBYp4ozB2xHorh5TcTKIaNcoFU0g",
+  authDomain: "crwn-db-309ae.firebaseapp.com",
+  projectId: "crwn-db-309ae",
+  storageBucket: "crwn-db-309ae.appspot.com",
+  messagingSenderId: "1013707198180",
+  appId: "1:1013707198180:web:3d1d1b080d132e244a66f1",
+  measurementId: "G-LM2QTJE0TK"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
