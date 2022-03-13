c2c4de742765dc989bb9fec7c538b83435307653
Deepti Aggarwal <er.deepti.aggarwal@outlook.com>
2022-03-14 01:06:37 +0530

    Complete Redux Thunk Pattern- Async, Started Container Pattern

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -21,3 +21,13 @@ export const selectCollection = memoize(collectionUrlParam => createSelector(
     collections ? collections[collectionUrlParam] : null
   )
 );
+
+export const selectIsCollectionFetching = createSelector(
+  [selectShop],
+  shop => shop.isFetching
+);
+
+export const selectIsCollectionsLoaded = createSelector(
+  [selectShop],
+  shop => !!shop.collections
+);
