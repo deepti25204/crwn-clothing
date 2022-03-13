d34828ae55665822bed102ffffe144f2228d9481
Deepti Aggarwal <er.deepti.aggarwal@outlook.com>
2022-03-13 22:08:16 +0530

    Firestore settings- for automatically upload collections and documents to firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,16 +3,6 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 
-// const config = {
-//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-// };
-
 const config = {
   apiKey: "AIzaSyB4dv6XBYp4ozB2xHorh5TcTKIaNcoFU0g",
   authDomain: "crwn-db-309ae.firebaseapp.com",
@@ -49,6 +39,21 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
   return userRef;
 };
+
+export const addCollectionAndDocuments = async (
+  collectionKey, 
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+  
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+}
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
